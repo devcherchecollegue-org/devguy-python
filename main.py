efe085f211eab0bdb48db484584030474984d72b
amaury5432 <amauryval@gmail.com>
2021-02-21 08:26:04 +0100

    upd (discord_client.py) main class inherits discord Client

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from os import environ, path
-from dotenv import load_dotenv
-from app.discord_client import DiscordClient
 
+from dotenv import load_dotenv
+
+from app.discord_client import DiscordClient
 
 if environ.get("BOT_ENV") == "development":
     basedir = path.dirname(__file__)
     load_dotenv(path.join(basedir, '.env.development'))
 
-
-client = DiscordClient(environ.get("DISCORD_BOT_SECRET_KEY"))
-client.run()
+client = DiscordClient()
+client.run(environ.get("DISCORD_BOT_SECRET_KEY"))
