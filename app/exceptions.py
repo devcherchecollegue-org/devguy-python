e6eb0a3356e14bf44ed0d6e46f3ed69dcf4c6849
PhilippeGalvan <git.philippeG@outlook.com>
2021-02-25 16:52:37 +0100

    First version proto1

---
+++ app/exceptions.py
@@ -0,0 +1,14 @@
+class RoleDoesNotExists(Exception):
+    pass
+
+
+class GuildDoesNotExists(Exception):
+    pass
+
+
+class MemberDoesNotExists(Exception):
+    pass
+
+
+class InvalidReactionType(Exception):
+    pass
