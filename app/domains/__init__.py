e55fbd7e41ffc1f9019368054582d492810e2a2f
tfreville <tfreville@tankyou.co>
2021-03-10 13:22:18 +0100

    docs: Update readme and module docstring
    Trying to explain architecture with readme and docstrings ~~

--- app/domains/__init__.py
+++ app/domains/__init__.py
@@ -1,2 +1,7 @@
+"""
+Domains objects represent all items used
+within the bot and shared from transport to modules.
+"""
+
 from .exceptions import *
 from .reaction import *
---
+++ app/modules/__init__.py
@@ -0,0 +1,6 @@
+"""
+Modules regroup all implementations who do things.
+It is the place to describe communication with database,
+notifications to other system places, operation asked by
+the usecases.
+"""
